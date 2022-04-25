945c53874a48c8efa7af63bcd2cab4a54f677b2c
Bin Xiao <bxiao5050@gmail.com>
2022-04-25 15:10:34 +0200

    de

--- authority_ssm_dao/src/main/java/com/shulei/ssm/dao/IMemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.shulei.ssm.dao;
-
-import com.shulei.ssm.domain.Member;
-import org.apache.ibatis.annotations.Select;
-
-public interface IMemberDao {
-    @Select("select * from member where id = #{id}")
-    public Member findById(String id) throws Exception;
-}
